7978c6ce61375481d7ce04419f28842bd45ff14e
spam5041 <forspam5041@gmail.com>
2024-09-03 17:35:53 +0300

    Create raydium.js

---
+++ utils/raydium.js
@@ -0,0 +1,9 @@
+exports.performDexTransaction = async (amount_token, type, asset_id) => {
+  // Imitation of interaction with Raydium dex
+  return 'test-dex-transaction-id';
+};
+
+exports.getTokenPrice = async (asset_id) => {
+  // Imitation of interaction with Raydium dex
+  return 10.0; // Example: Return the current price of token
+};
